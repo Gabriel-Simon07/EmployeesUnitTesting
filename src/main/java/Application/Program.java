e2ef86a5f8337a7306b22742fc42adb41f56b2fd
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-01 20:28:50 -0300

    Implemented codes for testing

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -10,17 +10,17 @@ import Entities.Employee;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("How many employees will be registered ?");
 		int employees = sc.nextInt();
-		
+
 		List<Employee> listEmployee = new ArrayList<Employee>();
-		
-		for(int i=0; i < employees; i++) {
-			System.out.println("Employee #" + i+1 +":");
+
+		for (int i = 0; i < employees; i++) {
+			System.out.println("Employee #" + (i + 1) + ":");
 			System.out.print("Id: ");
 			Integer id = sc.nextInt();
 			System.out.print("Name: ");
@@ -28,16 +28,40 @@ public class Program {
 			String name = sc.nextLine();
 			System.out.print("Salary: ");
 			Double salary = sc.nextDouble();
-		
+
 			Employee emp = new Employee(id, name, salary);
-			
+
 			listEmployee.add(emp);
 		}
-		
+
 		System.out.print("Enter the employee id that will have salary increase: ");
-		
-		
+		int idSalary = sc.nextInt();
+		Integer pos = positionIdList(listEmployee, idSalary);
+
+		if (pos == null) {
+			System.out.println("This is does not exist!");
+		} else {
+			System.out.println("Enter the porcentage: ");
+			double percent = sc.nextDouble();
+			listEmployee.get(pos).increaseSalary(percent);
+		}
+
+		System.out.println("\nList of employees: ");
+
+		for (Employee employee : listEmployee) {
+			System.out.println(employee);
+		}
+
 		sc.close();
+	}
+
+	public static Integer positionIdList(List<Employee> list, int id) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
 	}
 
 }
--- src/main/java/Entities/Employee.java
+++ src/main/java/Entities/Employee.java
@@ -39,4 +39,9 @@ public class Employee {
 	public void increaseSalary(double percentage) {
 		salary += salary * percentage / 100;
 	}
+
+	@Override
+	public String toString() {
+		return id + ", name=" + name + ", " + String.format("%.2f", salary);
+	}
 }
